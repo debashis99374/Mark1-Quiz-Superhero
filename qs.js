b56e7363bb99d92a45a31e3f778e66e98f56931e
Debashis Kar <116684191+debashis99374@users.noreply.github.com>
2022-11-24 23:15:39 +0530

    Update qs.js

--- qs.js
+++ qs.js
@@ -1,45 +1,73 @@
 var score=0;
-var userName=prompt('may i know your name?= ')
-console.log('Welcome '+userName+' to Batman Quiz by Fandome')
-console.log('---------------------------------------------')
+var response="";
 
-function play(question,answer) {
-  var userAnswer=prompt(question);
-  if (userAnswer===answer){
-    console.log('correct')
-    score+=1
-  }else{
-    console.log('wrong')
-  }
-  console.log(score)
-  console.log('---------------')
-  
-}
- var questionOne={
-  question:'what is Batmans Real name= ',
-  answer:'Bruce wayne'
+
+console.log("this is a quiz about batman");
+var userName=prompt("what may i call you sir?=");
+console.log("welcome "+userName+" to batman quiz");
+console.log("---------------------");
+ function play(question,answer){
+   var userAnswer=prompt(question);
+   if(userAnswer===answer){
+      response="Correct";
+     score+=1;
+   }else{
+     response="wrong"
+   }
+   console.log(response);
+   console.log("score is = "+ score);
+   console.log("---------------------");
+ };
+
+var questionOne={
+  question:"whats Batmans true name?=",
+  answer:"Bruce Wayne"
 };
-var questionTwo={
-  question:'what is batmans cave name is = ',
-  answer:'Batcave'
+  var questionTwo={
+  question:"whats Batmans fathers name?",
+  answer:"Thomas Wayne"
 };
 var questionThree={
-  question:'how many robins did Batman work with so far = ',
+  question:'how many robins did Batman work with so far ? ',
   answer:'4'
 };
+
+
+
 var questionFour={
-  question:'Who is the arch enemy of batman ',
+  question:'Who is the arch enemy of batman? ',
   answer:'Joker'
 };
 var questionFive={
-  question:'Does Batman kills=',
+  question:'Does Batman kills?',
   answer:'No'
 };
-var a=[questionOne,questionTwo,questionThree,questionFour,questionFive];
-for(let i=0;i<a.length;i++){
-var currentQuestion=a[i];
-  play(currentQuestion.question,currentQuestion.answer)
-}
-console.log('---------------')
-console.log('thank you for your time   ')
-console.log('have a nice day!!')
+var questionSix={
+  question:'Does Batman uses Guns?',
+  answer:'No'
+};
+var questionSeven={
+  question:'What is Batmans Mother s name?' ,
+  answer:'Martha Wayne'
+};
+var questionEight={
+  question:'What is Batmans Buttler s name?',
+  answer:'Alfred'
+};
+var questionNine={
+  question:'Is Bruce Orphan?',
+  answer:'Yes'
+};
+var questionTen={
+  question:'Is Batman the Greatest Superhero?',
+  answer:'Yes'
+};
+  var listOfQuestions=[questionOne,questionTwo,questionThree,questionFour,questionFive,questionSix,questionSeven,questionEight,questionNine,questionTen];
+ for(let i=0;i<listOfQuestions.length;i++){
+   
+   play(listOfQuestions[i].question,listOfQuestions[i].answer);
+ }
+
+console.log("Thank you for your Time "+userName+" Sir");
+   console.log("Have aNice Day!!!");
+console.log("--------------------------END----------------------------------------------------------------------------------------------")
